d32f0c7b29dac3664e341f8d8aa961c93e031824
Andreas Michelakis <a.m.michelakis@gmail.com>
2021-12-15 16:01:38 +0200

    added scroll-to-top functionality for #47

--- assets/scripts/advanceScripts.js
+++ assets/scripts/advanceScripts.js
@@ -37,3 +37,38 @@ class ClickIt extends HTMLElement {
 }
 
 customElements.define("click-element", ClickIt);
+
+/**
+ * Select elements using querySelectorAll
+ * Add window.scrollTo(top) on click
+ *
+ * @class ScrollToTop
+ * @extends {HTMLElement}
+ */
+ class ScrollToTop extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+  }
+  static get observedAttributes() {
+    return ["querySelector"];
+  }
+  connectedCallback() {
+    this.addListener(this.getAttribute("querySelector"));
+  }
+
+  addListener(querySelector) {
+    let elements = document.querySelectorAll(querySelector);
+    for (let index = 0; index < elements.length; index++) {
+      const element = elements[index];
+      element.addEventListener("click", function (event) {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      });
+    }
+  }
+}
+customElements.define("scroll-to-top", ScrollToTop);
